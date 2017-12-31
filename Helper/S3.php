9a4cedffdcd22e3c22d5068f540d9685d3f3a19e
Thai Phan <thai@outlook.com>
2017-12-31 18:38:56 +1100

    Add missing available S3 regions

--- Helper/S3.php
+++ Helper/S3.php
@@ -31,12 +31,28 @@ class S3
                 'label' => 'Asia Pacific (Seoul)'
             ],
             [
+                'value' => 'ap-south-1',
+                'label' => 'Asia Pacific (Mumbai)'
+            ],
+            [
                 'value' => 'ap-southeast-1',
                 'label' => 'Asia Pacific (Singapore)'
             ],
             [
                 'value' => 'ap-southeast-2',
                 'label' => 'Asia Pacific (Sydney)'
+            ],
+            [
+                'value' => 'ca-central-1',
+                'label' => 'Canada (Central)'
+            ],
+            [
+                'value' => 'cn-north-1',
+                'label' => 'China (Beijing)'
+            ],
+            [
+                'value' => 'cn-northwest-1',
+                'label' => 'China (Ningxia)'
             ],
             [
                 'value' => 'eu-central-1',
@@ -51,12 +67,20 @@ class S3
                 'label' => 'EU (London)'
             ],
             [
+                'value' => 'eu-west-3',
+                'label' => 'EU (Paris)'
+            ],
+            [
                 'value' => 'sa-east-1',
                 'label' => 'South America (Sao Paulo)'
             ],
             [
                 'value' => 'us-east-1',
                 'label' => 'US East (N. Virginia)'
+            ],
+            [
+                'value' => 'us-east-2',
+                'label' => 'US East (Ohio)'
             ],
             [
                 'value' => 'us-west-1',
