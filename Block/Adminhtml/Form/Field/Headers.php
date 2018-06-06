f0b15f338f7471f17f465fcd68d465b448dea617
Thai Phan <thai@outlook.com>
2018-06-06 22:27:09 +1000

    Add a config option to add custom headers

---
+++ Block/Adminhtml/Form/Field/Headers.php
@@ -0,0 +1,34 @@
+<?php
+namespace Thai\S3\Block\Adminhtml\Form\Field;
+
+class Headers extends \Magento\Config\Block\System\Config\Form\Field\FieldArray\AbstractFieldArray
+{
+    /**
+     * @var \Magento\Framework\Data\Form\Element\Factory
+     */
+    protected $_elementFactory;
+
+    /**
+     * @param \Magento\Backend\Block\Template\Context $context
+     * @param \Magento\Framework\Data\Form\Element\Factory $elementFactory
+     * @param array $data
+     */
+    public function __construct(
+        \Magento\Backend\Block\Template\Context $context,
+        \Magento\Framework\Data\Form\Element\Factory $elementFactory,
+        array $data = []
+    )
+    {
+        $this->_elementFactory = $elementFactory;
+        parent::__construct($context, $data);
+    }
+
+    protected function _construct()
+    {
+        $this->addColumn('header', ['label' => __('Header')]);
+        $this->addColumn('value', ['label' => __('Value')]);
+        $this->_addAfter = false;
+        $this->_addButtonLabel = __('Add Custom Header');
+        parent::_construct();
+    }
+}
